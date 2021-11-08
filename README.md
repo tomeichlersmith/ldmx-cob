f61ff51c5198d7f5be806fa701616c119ddac5bc
Thomas Eichlersmith <eichl008@cmslab1.umncmslab>
2021-11-08 09:36:52 -0600

    note on how to get started, cleanup up env script

--- README.md
+++ README.md
@@ -5,4 +5,11 @@ Notes and helper scripts for working with LDMX COBs.
 ## Setup
 Similar to this [tracking dotfiles tutorial](https://www.atlassian.com/git/tutorials/dotfiles).
 
+Define where your `cob` working directory is going to be.
+In my work, I have it in `~/cob`. Then
+
+```
+mkdir ~/cob
+git clone --bare <this-repo> $HOME/cob/.git
+```
 
--- env.sh
+++ env.sh
@@ -1,12 +1,17 @@
 
 export dtm_ip=192.168.28.100
 export shmm_ip=192.168.1.35
-
-source /home/eichl008/cob/V3.4.1/i86-linux-64/tools/envs-sdk.sh || return $?
-export PS1="\[$(tput setaf 5)\][cob]\[$(tput sgr0)\] ${PS1}"
-
 export cob_shelf=13
+
+if [[ -z ${COB_HOME} ]]; then
+  export COB_HOME="$(dirname ${BASH_SOURCE[0]})"
+fi
+
+source ${COB_HOME}/cob/V3.4.1/i86-linux-64/tools/envs-sdk.sh || return $?
+export PS1="\[$(tput setaf 5)\][cob]\[$(tput sgr0)\] ${PS1}"
 
 reboot_cob_shelf() {
   ssh root@${shmm_ip} "clia deactivate ${cob_shelf} && sleep 5 && clia activate ${cob_shelf}"
 }
+
+alias cob-git='git --git-dir=${COB_HOME}/.git --work-tree=${COB_HOME}'
