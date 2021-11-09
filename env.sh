3f2be3f7891586369c3d241ee9912c7efc7da056
Tom Eichlersmith <eichl008@umn.edu>
2021-11-09 11:25:38 -0600

    add ldmx-daq sw install path to ld lib path

--- env.sh
+++ env.sh
@@ -11,6 +11,10 @@ if [[ -z ${COB_HOME} ]]; then
   export COB_HOME="$(dirname ${BASH_SOURCE[0]})"
 fi
 
+if [[ -d ${COB_HOME}/ldmx-daq ]]; then
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${COB_HOME}/ldmx-daq/software/install/lib
+fi
+
 source ${COB_HOME}/sdk/i86-linux-64/tools/envs-sdk.sh || return $?
 export PS1="\[$(tput setaf 5)\][cob]\[$(tput sgr0)\] ${PS1}"
 
