bb1ac025ef9e94ac784770ac448ac025bf168f7f
Tom Eichlersmith <eichl008@umn.edu>
2022-01-13 09:45:37 -0600

    allow user to choose if using stable or dev branch of ldmx-daq

--- env.sh
+++ env.sh
@@ -4,7 +4,7 @@ export shmm_ip=192.168.1.35
 export cob_shelf=13
 
 if [[ -z ${ROGUE_DIR} ]]; then
-  source /opt/rogue/setup_rogue.sh
+  source /opt/rogue/setup_rogue.sh || return $?
 fi
 
 if [[ -z ${COB_HOME} ]]; then
@@ -12,19 +12,25 @@ if [[ -z ${COB_HOME} ]]; then
   export COB_HOME="$(realpath ${COB_HOME})"
 fi
 
-if [[ -f ${COB_HOME}/ldmx-daq/software/pflib/Makefile ]]; then
-  # we are on Jeremy's branch with the Makefile
+if [ "$1" == "dev" ]; then
+  # ldmx-daq-dev is my (development) branch
+  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${COB_HOME}/ldmx-daq-dev/software/install/lib
+  export PYTHONPATH=${PYTHONPATH}:${COB_HOME}/ldmx-daq-dev/software/install/lib
+  export PATH=${PATH}${COB_HOME}/ldmx-daq-dev/software/install/bin
+  tag='cob-dev'
+else
+  # ldmx-daq is Jeremy's (more stable) branch
+  #  so it is the default
   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${COB_HOME}/ldmx-daq/software/pflib/lib
   alias tool='${COB_HOME}/ldmx-daq/software/pflib/tool/pftool.exe'
-else
-  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${COB_HOME}/ldmx-daq/software/install/lib
-  export PYTHONPATH=${PYTHONPATH}:${COB_HOME}/ldmx-daq/software/install/lib
+  tag='cob'
 fi
 
 if [[ -d ${COB_HOME}/sdk ]]; then
-  source ${COB_HOME}/sdk/i86-linux-64/tools/envs-sdk.sh
+  source ${COB_HOME}/sdk/i86-linux-64/tools/envs-sdk.sh || return $?
 fi
-export PS1="\[$(tput setaf 5)\][cob]\[$(tput sgr0)\] ${PS1}"
+export PS1="\[$(tput setaf 5)\][${tag}]\[$(tput sgr0)\] ${PS1}"
+unset tag
 
 reboot_cob_shelf() {
   ssh root@${shmm_ip} "clia deactivate ${cob_shelf} && sleep 5 && clia activate ${cob_shelf}"
