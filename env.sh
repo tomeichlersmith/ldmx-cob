70679ea1deb7e44591583111902e026d31e190bc
Tom Eichlersmith <eichl008@umn.edu>
2022-01-25 08:46:29 -0600

    patch reboot function, need board parameter

--- env.sh
+++ env.sh
@@ -47,7 +47,7 @@ export PS1="\[$(tput setaf 5)\][${tag}]\[$(tput sgr0)\] ${PS1}"
 unset tag
 
 reboot_cob_shelf() {
-  ssh root@${shmm_ip} "clia deactivate ${cob_shelf} && sleep 5 && clia activate ${cob_shelf}"
+  ssh root@${shmm_ip} "clia deactivate board ${cob_shelf} && sleep 5 && clia activate board ${cob_shelf}"
 }
 
 raw2txt() {
