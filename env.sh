a4bd7dfbf7748e54c95515c56b1519b154a4fab7
Tom Eichlersmith <eichl008@umn.edu>
2022-01-26 08:29:08 -0600

    cleanup env script to be more robust

--- env.sh
+++ env.sh
@@ -23,32 +23,20 @@ fi
 if [[ -z ${COB_HOME} ]]; then
   COB_HOME="$(dirname ${BASH_SOURCE[0]})"
   export COB_HOME="$(realpath ${COB_HOME})"
-fi
-
-if [ "$1" == "dev" ]; then
-  # ldmx-daq-dev is my (development) branch
-  export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${COB_HOME}/ldmx-daq-dev/software/install/lib
-  export PYTHONPATH=${PYTHONPATH}:${COB_HOME}/ldmx-daq-dev/software/install/lib
-  export PATH=${PATH}:${COB_HOME}/ldmx-daq-dev/software/install/bin
-  tag='cob-dev'
-else
-  # ldmx-daq is Jeremy's (more stable) branch
-  #  so it is the default
+  # add ldmx-daq/software install directories to the env variables
   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${COB_HOME}/ldmx-daq/software/install/lib
   export PYTHONPATH=${PYTHONPATH}:${COB_HOME}/ldmx-daq/software/install/lib
   export PATH=${PATH}:${COB_HOME}/ldmx-daq/software/install/bin
-  tag='cob'
+  # add [cob] tag to shell prompt
+  export PS1="\[$(tput setaf 5)\][cob]\[$(tput sgr0)\] ${PS1}"
+  # if SDK exists here, source its environment
+  if [[ -d ${COB_HOME}/sdk ]]; then
+    source ${COB_HOME}/sdk/i86-linux-64/tools/envs-sdk.sh || return $?
+    reboot_cob_shelf() {
+      ssh root@${shmm_ip} "clia deactivate board ${cob_shelf} && sleep 5 && clia activate board ${cob_shelf}"
+    }
+  fi
 fi
-
-if [[ -d ${COB_HOME}/sdk ]]; then
-  source ${COB_HOME}/sdk/i86-linux-64/tools/envs-sdk.sh || return $?
-fi
-export PS1="\[$(tput setaf 5)\][${tag}]\[$(tput sgr0)\] ${PS1}"
-unset tag
-
-reboot_cob_shelf() {
-  ssh root@${shmm_ip} "clia deactivate board ${cob_shelf} && sleep 5 && clia activate board ${cob_shelf}"
-}
 
 raw2txt() {
   hexdump -v -e '1/4 "%08x " "\n"' $@
